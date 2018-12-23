924ff1e3316e4576526337752f1d967bcab8bc7a
limatgans <limatgans@gmail.com>
2018-12-24 00:01:53 +0530

    [styling html] Changes in Look and Feel

--- src/js/cutime.js
+++ src/js/cutime.js
@@ -1,7 +1,11 @@
 "use strict";
 
 const siTimeHeader = document.getElementById("SITime");
-const cuTimeHeader = document.getElementById("CUTime");
+// const cuTimeHeader = document.getElementById("CUTime");
+const kiloClarkeDiv = document.getElementById("kiloClarke"); 
+const hectoClarke = document.getElementById("hectoClarke");
+const clarke = document.getElementById("clarke");
+
 const interval = 500;
 const dayInSeconds = 86400;
 
@@ -16,10 +20,33 @@ const cuTimeConversion = (dateToBeConverted) => {
 	return kiloClarkes.toFixed(4);
 }
 
+const splitKiloClarke = (dateToBeConverted) => {
+	const kiloClarkeInDecimals = cuTimeConversion(dateToBeConverted);
+	const tempArray = kiloClarkeInDecimals.split('.');
+	return tempArray;
+}
+
+const getKiloClarke = (dateToBeConverted) => {
+	const temp = splitKiloClarke(dateToBeConverted);
+	return '0' + temp[0] + ".";
+}
+
+const getHectoClarke = (dateToBeConverted) => {
+	const tempArray = splitKiloClarke(dateToBeConverted);
+	return tempArray[1][0] + tempArray[1][1];
+}
+
+const getClarke = (dateToBeConverted) => {
+	const tempArray = splitKiloClarke(dateToBeConverted);
+	return tempArray[1][2] + tempArray[1][3];;
+}
+
 setInterval(() => {
 	const nowDate = new Date();
 	siTimeHeader.innerText = nowDate.toLocaleString();
-	cuTimeHeader.innerText = cuTimeConversion(nowDate) + " kiloclarkes";
-
+	// cuTimeHeader.innerText = cuTimeConversion(nowDate) + " kiloclarkes";
+	kiloClarkeDiv.innerText = getKiloClarke(nowDate);
+	hectoClarke.innerText = getHectoClarke(nowDate);
+	clarke.innerText = getClarke(nowDate);
 }, interval);
 

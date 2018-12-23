5a26cc6ed8d3aa06db69bb500db5ec4f471dc315
limatgans <limatgans@gmail.com>
2018-12-24 00:22:21 +0530

    [refactor] cutime.js

--- src/js/cutime.js
+++ src/js/cutime.js
@@ -1,10 +1,9 @@
 "use strict";
 
 const siTimeHeader = document.getElementById("SITime");
-// const cuTimeHeader = document.getElementById("CUTime");
-const kiloClarkeDiv = document.getElementById("kiloClarke"); 
-const hectoClarke = document.getElementById("hectoClarke");
-const clarke = document.getElementById("clarke");
+const kiloClarkeHeader = document.getElementById("kiloClarke"); 
+const hectoClarkeHeader = document.getElementById("hectoClarke");
+const clarkeHeader = document.getElementById("clarke");
 
 const interval = 500;
 const dayInSeconds = 86400;
@@ -17,36 +16,20 @@ const cuTimeConversion = (dateToBeConverted) => {
 
 	// Conversion Done Here
 	const kiloClarkes = (timeInSeconds / dayInSeconds) * 10; 
-	return kiloClarkes.toFixed(4);
-}
-
-const splitKiloClarke = (dateToBeConverted) => {
-	const kiloClarkeInDecimals = cuTimeConversion(dateToBeConverted);
-	const tempArray = kiloClarkeInDecimals.split('.');
-	return tempArray;
-}
-
-const getKiloClarke = (dateToBeConverted) => {
-	const temp = splitKiloClarke(dateToBeConverted);
-	return '0' + temp[0] + ".";
-}
-
-const getHectoClarke = (dateToBeConverted) => {
-	const tempArray = splitKiloClarke(dateToBeConverted);
-	return tempArray[1][0] + tempArray[1][1];
-}
-
-const getClarke = (dateToBeConverted) => {
-	const tempArray = splitKiloClarke(dateToBeConverted);
-	return tempArray[1][2] + tempArray[1][3];;
+	const tempArray = kiloClarkes.toFixed(4).split(".");
+	return {
+		kiloClarke : "0" + tempArray[0] + ".",
+		hectoClarke: tempArray[1][0] + tempArray[1][1],
+		clarke: tempArray[1][2] + tempArray[1][3] 
+	}
 }
 
 setInterval(() => {
 	const nowDate = new Date();
 	siTimeHeader.innerText = nowDate.toLocaleString();
-	// cuTimeHeader.innerText = cuTimeConversion(nowDate) + " kiloclarkes";
-	kiloClarkeDiv.innerText = getKiloClarke(nowDate);
-	hectoClarke.innerText = getHectoClarke(nowDate);
-	clarke.innerText = getClarke(nowDate);
+	const {kiloClarke, hectoClarke, clarke} = cuTimeConversion(nowDate)
+	kiloClarkeHeader.innerText = kiloClarke;
+	hectoClarkeHeader.innerText = hectoClarke;
+	clarkeHeader.innerText = clarke;
 }, interval);
 

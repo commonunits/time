7ae3934162cc40a1255a1c514dbb69b26351e8d0
Ceda EI <ceda_ei@protonmail.com>
2019-01-06 12:25:58 +0530

    Switch to printf to display decimals correctly

--- polybar/clarke-time/clarke-time.sh
+++ polybar/clarke-time/clarke-time.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 seconds=$(($(date +%-H)*3600 + $(date +%-M)*60 + $(date +%-S)))
-clarkes=$(printf '%05d' $(($seconds*100000/86400)))
-echo $(($clarkes/1000)).$(($clarkes%1000)) kϾ
+clarkes=$(($seconds*100000/86400))
+printf '%02d.%03d kϾ' $(($clarkes/1000)) $(($clarkes%1000))
